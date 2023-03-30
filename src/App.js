6d731499888fd30270c2827d56da9c889ec90ef1
Dmitriy Yanushkevich <dmitriy.ya@protonmail.com>
2023-03-30 13:28:39 +0300

    Change adding 'slide-in' class to declarative way
    Change adding 'slide-in' class to declarative (more reactive) way

--- src/App.js
+++ src/App.js
@@ -19,24 +19,12 @@ function App() {
     return () => observer.disconnect();
   }, [isIntersecting]);
 
-  useEffect(() => {
-    if (isIntersecting) {
-      ref.current.querySelectorAll("div").forEach((el) => {
-        el.classList.add("slide-in");
-      });
-    } else {
-      ref.current.querySelectorAll("div").forEach((el) => {
-        el.classList.remove("slide-in");
-      });
-    }
-  }, [isIntersecting]);
-
   return (
     <div className="App">
       <header>This is the Header</header>
       <main ref={ref}>
-        <div className="child-one">Child One</div>
-        <div className="child-two">Child Two</div>
+        <div className={`child-one ${isIntersecting ? 'slide-in' : ''}`}>Child One</div>
+        <div className={`child-two ${isIntersecting ? 'slide-in' : ''}`}>Child Two</div>
       </main>
       <footer>This is the Footer</footer>
     </div>
